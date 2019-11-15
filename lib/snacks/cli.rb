07b9a3624b1dccd5c5d7bd15c94eaae86394341c
RachelCodesToo <a.r.fausto@gmail.com>
2019-11-14 17:19:25 -0800

    added some instance methods in the Cli class

--- lib/snacks/cli.rb
+++ lib/snacks/cli.rb
@@ -1,6 +1,24 @@
 class Cli 
 
-    puts "this program is working"
+    def call 
+        #welcome the user to snacks
+        #show the user all the items in the SNACKLIST categories
+        #ask the user to select a SNACKLIST category to learn more about 
+        #get input from user to show more info about the snack 
+        welcome
+        snacklist
+        select_snack
+        snacklist 
+    end 
+
+    def welcome
+        puts "Welcome to Snacks!"
+        puts "Here you will find the variety of snacks available on Nuts.com and their nutritional facts."
+    end 
+
+
+
+
 
 
 end
