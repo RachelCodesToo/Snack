077f3f3d16594cfea0d72b4c75c34dc11ea75632
RachelCodesToo <a.r.fausto@gmail.com>
2019-11-15 05:38:24 -0800

    pseudo code before parsing data

--- lib/snacks/cli.rb
+++ lib/snacks/cli.rb
@@ -6,9 +6,9 @@ class Cli
         #ask the user to select a SNACKLIST category to learn more about 
         #get input from user to show more info about the snack 
         welcome
-        show_snacklist
+        show_snacklists
         select_snack
-        snacklist 
+        show_snacklist 
     end 
 
     def welcome
@@ -16,7 +16,7 @@ class Cli
         puts "Here you will find the variety of snacks available on Nuts.com and their nutritional facts."
     end 
 
-    def show_snacklist 
+    def show_snacklists
         puts "1. dried fruits"
         puts "2. trail mix"
         puts "3. mixed nuts"
@@ -25,18 +25,26 @@ class Cli
     def select_snack
         "Which Snack do you want to learn more about?"
         input = gets.chomp
-        if input > 0 && input < 4 #input_invalid(input)
-            show_snack
+        if !input_valid(input)  # input > 0 && input < 4
+            select_snack
         else 
-            show_snacklist
+            show_snacklists
         end 
     end 
 
-    # def input_invalid(input)
-    #   input > 0 && input < 4 
-    # end 
+    def input_valid(input)
+      input > 0 && input < 4 
+    end 
 
-    
+    def show_snacklist
+        puts "this is the info of the snack you selected"
+
+    end 
+
+
+
+
+
 
 
 end
