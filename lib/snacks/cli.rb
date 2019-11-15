cf7a9c3f6cc7df9e4df07b8afec04c20b4894a52
RachelCodesToo <a.r.fausto@gmail.com>
2019-11-14 18:21:09 -0800

    defined some instance methods in cli

--- lib/snacks/cli.rb
+++ lib/snacks/cli.rb
@@ -6,7 +6,7 @@ class Cli
         #ask the user to select a SNACKLIST category to learn more about 
         #get input from user to show more info about the snack 
         welcome
-        snacklist
+        show_snacklist
         select_snack
         snacklist 
     end 
@@ -16,9 +16,27 @@ class Cli
         puts "Here you will find the variety of snacks available on Nuts.com and their nutritional facts."
     end 
 
+    def show_snacklist 
+        puts "1. dried fruits"
+        puts "2. trail mix"
+        puts "3. mixed nuts"
+    end 
 
+    def select_snack
+        "Which Snack do you want to learn more about?"
+        input = gets.chomp
+        if input > 0 && input < 4 #input_invalid(input)
+            show_snack
+        else 
+            show_snacklist
+        end 
+    end 
 
+    # def input_invalid(input)
+    #   input > 0 && input < 4 
+    # end 
 
+    
 
 
 end
